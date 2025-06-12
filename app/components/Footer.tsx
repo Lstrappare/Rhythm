0a8f05fece710147d5fcc421d7bd3e83e1887723
Lstrappare <j.m.cisval1@gmail.com>
2025-06-12 16:33:28 -0600

    Footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,10 +1,23 @@
-
 export default function Footer () {
   return (
-    <footer className="text-center text-sm text-gray-400 py-6 border-t mt-12">
-      <p>© 2025 José Manuel Cisneros. All rights reserved.</p>
-      <p>This project was developed for educational purposes only and is not intended for commercial use. Some songs used may be protected by copyright.</p>
-      <p>Contact: <a href="mailto:j.m.cisval1@gmail.com" className="text-blue-500 hover:underline">j.m.cisval1@gmail.com</a></p>
+    <footer className="text-center text-sm text-gray-300/50 py-6 border-t mt-12">
+      <p>
+        © 2025 José Manuel Cisneros.{' '}
+        <span
+          className="text-cyan-500/50"
+        >
+          MIT License
+        </span>
+      </p>
+      <p>
+        This project was developed for educational purposes only and is not intended for commercial use.
+      </p>
+      <p>
+        Some audio content may be copyrighted and is used for demonstration only.
+      </p>
+      <p>
+        Contact: <a href="mailto:j.m.cisval1@gmail.com" className="text-cyan-500/50 hover:underline">j.m.cisval1@gmail.com</a>
+      </p>
     </footer>
   );
 }
